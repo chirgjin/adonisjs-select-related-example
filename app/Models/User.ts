bdef6ccadb5bc8499e24db16810a009c025b63d3
chirgjin <chirgjin@gmail.com>
2022-02-12 01:47:56 +0530

    feat: create user model & migration

---
+++ app/Models/User.ts
@@ -0,0 +1,21 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { compose } from '@ioc:Adonis/Core/Helpers'
+import { selectRelatedMixin } from '@ioc:Adonis/Addons/SelectRelated'
+
+export default class User extends compose(BaseModel, selectRelatedMixin) {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public email: string
+
+  @column()
+  public name: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
