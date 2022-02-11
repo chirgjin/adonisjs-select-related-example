62eed93508c0e316796eee1675962467de8cddfc
chirgjin <chirgjin@gmail.com>
2022-02-12 02:14:05 +0530

    feat: factories for generating random data

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,1 +1,35 @@
-// import Factory from '@ioc:Adonis/Lucid/Factory'
+import { User, Profile, UserTodoList, UserTodoListItem } from 'App/Models'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+import { DateTime } from 'luxon'
+
+export const UserTodoListItemFactory = Factory.define(UserTodoListItem, ({ faker }) => {
+  return {
+    itemName: faker.lorem.word(),
+    completed: faker.datatype.boolean(),
+  }
+}).build()
+
+export const UserTodoListFactory = Factory.define(UserTodoList, ({ faker }) => {
+  return {
+    title: faker.lorem.words(),
+  }
+})
+  .relation('items', () => UserTodoListItemFactory)
+  .build()
+
+export const ProfileFactory = Factory.define(Profile, ({ faker }) => {
+  return {
+    phoneNumber: faker.phone.phoneNumber(),
+    dateOfBirth: DateTime.fromJSDate(faker.date.past()),
+  }
+}).build()
+
+export const UserFactory = Factory.define(User, ({ faker }) => {
+  return {
+    name: faker.name.firstName(),
+    email: faker.internet.email(),
+  }
+})
+  .relation('profile', () => ProfileFactory)
+  .relation('todoLists', () => UserTodoListFactory)
+  .build()
